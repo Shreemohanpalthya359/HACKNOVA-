e1e749817bc92dd371ab613034a2f71652617ec8
Shree Mohan Chandra <shreemohanpalthya359@gmail.com>
2026-03-30 12:13:07 +0530

    Fix embedded backend repo and track files

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_cors import CORS
+from routes.api import api_bp
+
+def create_app():
+    app = Flask(__name__)
+
+    # Enable CORS
+    CORS(app)
+
+    # Register Blueprint ✅
+    app.register_blueprint(api_bp)
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
